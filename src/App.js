cfd2cd6b2efa665b80c15f0f179d02d1591671a9
ZachyDev <mosetizachary001@gmail.com>
2020-05-28 02:47:59 +0300

    react-icons

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { FaReact } from 'react-icons/fa'
+import { Md3DRotation } from 'react-icons/md'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Icons</h1>
+      <FaReact color ="purple" size = "10rem"/>
+      <Md3DRotation color ="purple" size = "10rem"/>
+    </div>
+  );
+}
+
+export default App;
